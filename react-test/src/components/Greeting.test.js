33b89597e01523a8a105d910a9caab1b9c0a788c
WeikunYe <weikunye1225@gmail.com>
2021-10-25 19:24:01 +1100

    react test

---
+++ react-test/src/components/Greeting.test.js
@@ -0,0 +1,38 @@
+import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+
+describe("Greeting component", () => {
+	test("renders Hello World as a text", () => {
+		// Arrange
+		render(<Greeting />);
+		const helloWorldElement = screen.getByText("Hello World");
+		expect(helloWorldElement).toBeInTheDocument();
+	});
+
+	test("renders good to see you if the button was NOT clicked", () => {
+		render(<Greeting />);
+		const outputElement = screen.getByText("good to see you", {
+			exact: false,
+		});
+		expect(outputElement).toBeInTheDocument();
+	});
+
+	test("renders Changed if the button WAS clicked", () => {
+		render(<Greeting />);
+		const buttonElement = screen.getByRole("button");
+		userEvent.click(buttonElement);
+		const outputElement = screen.getByText("Changed");
+		expect(outputElement).toBeInTheDocument();
+	});
+
+	test("does not renders 'good to see you' if the button WAS clicked", () => {
+		render(<Greeting />);
+		const buttonElement = screen.getByRole("button");
+		userEvent.click(buttonElement);
+		const outputElement = screen.queryByText("good to see you", {
+			exact: false,
+		});
+		expect(outputElement).toBeNull();
+	});
+});
